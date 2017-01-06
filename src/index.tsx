702435a19897ee9e236ca58ab794f56bc70b80ca
vvakame <vvakame+dev@gmail.com>
2017-01-06 13:05:48 +0900

    use tslib@1.5.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,4 @@
 import "tslib";
 import "skatejs-web-components";
 
-(window as any).__extends = function(d: any, b: any) {
-    Object.setPrototypeOf(d, b);
-    var __: any = function() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
-
 import "./view/todoAppComponent";
